b6aa04e45f04107c3bffba353854b8f1db968c78
Robert Mrowiec <rmrowiec@icloud.com>
2018-10-05 21:18:27 +0200

    added movies api without fetching yet

--- moviesApp.js
+++ moviesApp.js
@@ -1,4 +1,4 @@
-require('./app')('mongodb://Robert:qwe123@ds123753.mlab.com:23753/heroku_g4zdd28c').then(app => {
+require('./app')('mongodb://localhost/moviesapp').then(app => {
     console.log("Serwer działa na porcie : 8001")
-    app.listen(8001)
+    app.listen(process.env.PORT || 8001)
 })
