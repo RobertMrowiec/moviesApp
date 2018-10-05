5c044949ae5904cfc2bfbf3583729b1788d32762
Robert Mrowiec <rmrowiec@icloud.com>
2018-10-05 20:52:35 +0200

    update heroku port

--- moviesApp.js
+++ moviesApp.js
@@ -1,4 +1,4 @@
 require('./app')('mongodb://localhost/movieapp').then(app => {
     console.log("Server is running (locally) on port: 8001")
-    app.listen(process.env.MONGODB_URI)
+    app.listen(process.env.MONGODB_URI || 8001)
 })
